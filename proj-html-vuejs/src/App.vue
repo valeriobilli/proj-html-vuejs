1ebbc2b7ae77f4ddc4c9d4eb1c4f8a1a07d9dafc
Valerio Billi <valerio.billi18@gmail.com>
2023-05-30 16:46:12 +0200

    commenti sul layout

--- proj-html-vuejs/src/App.vue
+++ proj-html-vuejs/src/App.vue
@@ -2,6 +2,25 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template></template>
+<template>
+  <header>
+    <!-- navbar -->
+    <!-- jumbo -->
+  </header>
+
+  <main>
+    <!-- descrizione -->
+    <!-- corsi recenti -->
+    <!-- prossimi eventi -->
+    <!-- ultime dal nostro blog -->
+  </main>
+
+  <footer>
+    <!-- immagini -->
+    <!-- iscriviti alla newsletter -->
+    <!-- contatti -->
+    <!-- copyright -->
+  </footer>
+</template>
 
 <style scoped></style>
