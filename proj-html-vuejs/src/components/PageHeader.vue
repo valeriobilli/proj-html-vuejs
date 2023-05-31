60cae2cf39a767f72c6979b338b362a73b983441
Valerio Billi <valerio.billi18@gmail.com>
2023-05-31 17:45:25 +0200

    navbar

--- proj-html-vuejs/src/components/PageHeader.vue
+++ proj-html-vuejs/src/components/PageHeader.vue
@@ -14,14 +14,31 @@ export default {
 
 <template>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row d-flex justify-content-center align-items-center" id="navbar">
             <div class="col-8">
-                <div class="">
-                    <a v-for="link in navLinks">{{ link }}</a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <img src="../assets/img/Logo.jpg" alt="Logo" id="logoHeader">
+                    <div>
+                        <a v-for="link in navLinks">{{ link }}</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+#navbar {
+    height: 75px;
+    border-bottom: 1px solid black;
+}
+
+#logoHeader {
+    width: 145px;
+    height: 110px;
+}
+
+a {
+    margin-right: 1rem;
+}
+</style>
