82db360fe9164675b8775391705c251e633ca6cd
Valerio Billi <valerio.billi18@gmail.com>
2023-06-04 15:32:14 +0200

    nested component introduction (counter section)

--- proj-html-vuejs/src/components/PageMain.vue
+++ proj-html-vuejs/src/components/PageMain.vue
@@ -1,74 +1,28 @@
 <script>
+
+import introduction from './introduction.vue'
+
 export default {
-    name: "PageMain"
+    name: "PageMain",
+    components: {
+        introduction,
+    }
 }
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="row d-flex justify-content-center" id="presentation">
-            <div class="col-6">
-                <p>Since 2000, weve been partnering with education leaders to <span>increase equity and
-                        achievement</span>
-                    for all students.
-                </p>
-            </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-            <div class="col-8">
-                <!-- cards -->
-                <div class="row text-center">
-                    <div class="col-4">
-                        <div class="myCard text-center">
-                            <img src="../assets/svg/svg-0.svg" alt="">
-                            <div class="fw-bold">Online Coaching</div>
-                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
+    <introduction></introduction>
 
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="myCard text-center">
-                            <img src="../assets/svg/svg-1.svg" alt="">
-                            <div class="fw-bold">Consultaion</div>
-                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="myCard text-center">
-                            <img src="../assets/svg/svg-2.svg" alt="">
-                            <div class="fw-bold">Life Programs</div>
-                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
-                        </div>
-                    </div>
-                </div>
+    <!-- recent courses -->
+    <div class="row d-flex justify-content-center">
+        <div class="col-5">
+            <div class="row text-center">
+                <div>Recent courses</div>
+                <div>Whether you're considering a foundation course or an undergraduate degree master's or a PhD,
+                    Academics is a place where students thrive.</div>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss">
-#presentation {
-    text-align: center;
-    margin-bottom: 4rem;
-
-    p {
-        font-size: 26px;
-    }
-}
-
-.myCard {
-    width: 100%;
-    padding: 3rem;
-    box-shadow: 0px 21px 50px 8px rgba(0, 0, 0, 0.08);
-    margin-bottom: 3rem;
-
-    img {
-        width: 30%;
-        margin-bottom: 1.5rem;
-    }
-
-    div {
-        margin-bottom: 1.5rem;
-    }
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ proj-html-vuejs/src/components/introduction.vue
@@ -0,0 +1,168 @@
+<script>
+export default {
+    name: "introduction",
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center" id="presentation">
+            <div class="col-6">
+                <p>Since 2000, weve been partnering with education leaders to <span>increase equity and
+                        achievement</span>
+                    for all students.
+                </p>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8">
+                <div class="row text-center">
+                    <div class="col-4">
+                        <div class="myCard text-center">
+                            <img src="../assets/svg/svg-0.svg" alt="">
+                            <div class="fw-bold">Online Coaching</div>
+                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
+
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="myCard text-center">
+                            <img src="../assets/svg/svg-1.svg" alt="">
+                            <div class="fw-bold">Consultaion</div>
+                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="myCard text-center">
+                            <img src="../assets/svg/svg-2.svg" alt="">
+                            <div class="fw-bold">Life Programs</div>
+                            <p>Loren ipsum is simply dummy text of the print ing and typesetting has been the industrys.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="d-flex align-items-center ml-1">
+                            <div class="fw-bold fs-2 orange mr-1" id="counterStudents">6500</div>
+                            <div class="fw-bold fs-2 orange mr-1">+</div>
+                            <div class="fw-bold">Students</div>
+                        </div>
+                    </div>
+                    <div class="col-4 numbers">
+                        <div class="d-flex align-items-center ml-1">
+                            <div class="fw-bold fs-2 red mr-1" id="counterCourses">200</div>
+                            <div class="fw-bold fs-2 red mr-1">+</div>
+                            <div class="fw-bold">Online Courses</div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="d-flex align-items-center ml-1">
+                            <div class="fw-bold fs-2 green mr-1" id="counterClients">100</div>
+                            <div class="fw-bold fs-2 green mr-1">%</div>
+                            <div class="fw-bold">Happy Clients</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+#presentation {
+    text-align: center;
+    margin-bottom: 4rem;
+
+    p {
+        font-size: 26px;
+    }
+}
+
+.myCard {
+    width: 100%;
+    padding: 3rem;
+    box-shadow: 0px 21px 50px 8px rgba(0, 0, 0, 0.08);
+    margin-bottom: 3rem;
+
+    img {
+        width: 30%;
+        margin-bottom: 1.5rem;
+    }
+
+    div {
+        margin-bottom: 1.5rem;
+    }
+
+    &:hover div {
+        color: #D74D52;
+    }
+}
+
+#counterStudents {
+    background-image: url(../assets/img/counter-shape-1-1-1.png);
+    background-size: contain;
+}
+
+#counterCourses {
+    background-image: url(../assets/img/counter-shape-2-1-1.png);
+    background-size: contain;
+}
+
+#counterClients {
+    background-image: url(../assets/img/counter-shape-3-1-1.png);
+    background-size: contain;
+}
+
+.orange {
+    color: #F28202;
+}
+
+.red {
+    color: #D74D52;
+}
+
+.green {
+    color: #3ADC79;
+}
+
+.mr-1 {
+    margin-right: 1rem;
+}
+
+.ml-1 {
+    margin-left: 1rem;
+}
+
+
+
+
+
+
+
+// .stats {
+//     width: 100px;
+//     height: 70px;
+
+//     .counter {
+//         font-size: 2rem;
+//         height: 3.5rem;
+//         background-size: cover;
+//         width: 100%;
+//         height: 100%;
+//     }
+
+//     .orange {
+//         color: #F28202;
+//         background-image: url(../assets/img/counter-shape-1-1-1.png);
+//     }
+// }
+</style>
